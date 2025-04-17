5d622b2fba70e01c55096c06ad0f794c9fa43dfa
Aman Verma <amanv3985@gmail.com>
2025-04-17 14:44:30 +0530

    exploring get post

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+//this only match get http request
+app.get("/user",(req,res) =>{
+    res.send({firstName:"Aman", lastName:"verma"})
+})
+
+// this will match all http methods of all api call
 app.use("/test" , (req,res)=>{
     res.send("Hello server");
+})
+
+app.use("/" , (req,res)=>{
+    res.send("Aman");
 })
 
 app.listen(3000, (req,res)=>{
