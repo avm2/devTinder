467c778f0daf8657a385925866771b38750af216
Aman Verma <amanv3985@gmail.com>
2025-04-21 16:35:56 +0530

    API level Sanitization

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const {connectDB}=require("./config/database.js")
+const { connectDB } = require("./config/database.js");
 const User = require("./models/user.js");
 
 const app = express();
-const {adminAuth} = require("./middleware/auth.js")
-
-
+const { adminAuth } = require("./middleware/auth.js");
 
 //this middleware convert JSON to JS object
 app.use(express.json());
@@ -23,7 +21,6 @@ app.use(express.json());
 // app.use("/" , (req,res)=>{
 //     res.send("Aman");
 // })
-
 
 // app.get("/user" ,
 //      (req,res,next) =>{
@@ -44,19 +41,16 @@ app.use(express.json());
 // }
 // )
 
-
 // app.use("/admin",adminAuth);
 
 // app.get("/admin/getAllData" ,(req,res) =>{
 //     res.send("All data sent");
 // });
 
-
 //OR
 // app.get("/admin/getAllData" , adminAuth,(req,res) =>{
 //     res.send("All data sent");
 // });
-
 
 // app.get("/user" , (req,res,next)=>{
 //     console.log("first route");
@@ -68,77 +62,83 @@ app.use(express.json());
 //     res.send("2nd response");
 // })
 
-app.post("/signup" , async(req,res) =>{
+app.post("/signup", async (req, res) => {
+  //console.log(req.body)
 
-    //console.log(req.body)
-   
-   // creating a new instance os user model
-    const user = new User(req.body);
-    try{
-        await user.save();
-        res.send("User added successfully")
-    }catch(err){
-        res.status(400).send("Internal server error")
-    }
-
-})
+  // creating a new instance os user model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Internal server error");
+  }
+});
 
 //get one user
-app.get("/user" , async(req,res) =>{
-    const userName = req.body.firstName;
-    try{
-        const user= await User.find({firstName:userName});
-        res.send(user);
-    }catch(error){
-        res.status(400).send("Something went wrong")
-    }
-})
+app.get("/user", async (req, res) => {
+  const userName = req.body.firstName;
+  try {
+    const user = await User.find({ firstName: userName });
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 //feed API
-app.get("/feed" , async(req,res) =>{
-   
-    try{
-        const users= await User.find({});
-        res.send(users);
-    }catch(error){
-        res.status(400).send("Something went wrong")
-    }
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 // delete user
 
-app.delete("/user" , async(req,res) =>{
-    const userId= req.body.userId;
-    try{
-        //await User.findByIdAndDelete({_id:userId});
-        await User.findByIdAndDelete(userId);
-        res.send("User deleted successfully");
-    }catch(error){
-        res.status(400).send("Something went wrong");
-    }
-})
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    //await User.findByIdAndDelete({_id:userId});
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 //update user
 
-app.patch("/user", async(req,res) =>{
-    const userId = req.body.userId;
-    const data = req.body;
-    try{
-        await User.findByIdAndUpdate({_id:userId} ,data);
-        res.send("User updated successfully");
-    }catch(error){
-        res.status(400).send("something went wrong");
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATE = ["photoUrl", "about", "skills", "age"];
+
+    const isAllowed = Object.key(data).every((k) => {
+      ALLOWED_UPDATE.includes(k);
+    });
+
+    if (!isAllowed) {
+      res.status(400).send("Update is not allowed");
     }
-})
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
 
-connectDB().then(() =>{
+connectDB()
+  .then(() => {
     console.log("Database connected successfully");
-    app.listen(3000, (req,res)=>{
-        console.log("server running at 3000");
-    })
-}).catch(err=>{
-    console.log("Database not connected successfully")
-})
-
-
-
+    app.listen(3000, (req, res) => {
+      console.log("server running at 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Database not connected successfully");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -3,24 +3,45 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50
     },
     lastName:{
         type:String
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     gender:{
         type:String
+    },
+    photoUrl:{
+        type:String
+    },
+    skills:{
+        type:[String]
+    },
+    about:{
+        type:String,
+        default:"This is short description!",
+        minLength:100,
+        maxLength:400
     }
-});
+},{timeStamps:true});
 
 
 module.exports= mongoose.model("User",userSchema);
