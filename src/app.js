e3e4ca683f475bd4f545294f14eab9a3a106de24
Aman Verma <amanv3985@gmail.com>
2025-04-24 17:30:11 +0530

    getJWT

--- src/app.js
+++ src/app.js
@@ -110,7 +110,7 @@ app.post("/login", async(req,res) =>{
             throw new Error("Email id not present in DB");
         }
 
-        const isPasswordValid = await bcrypt.compare(password,user.password);
+        const isPasswordValid = await user.validatePassword(password)
 
         if(isPasswordValid){
 
